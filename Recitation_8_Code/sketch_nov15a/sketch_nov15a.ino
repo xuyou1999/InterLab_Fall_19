348f75e08e1927a4bb7a46412e47710f36d3ef46
You Xu <48320057+xuyou1999@users.noreply.github.com>
2019-11-17 16:47:24 +0800

    Add files via upload

---
+++ Recitation_8_Code/sketch_nov15a/sketch_nov15a.ino
@@ -0,0 +1,23 @@
+// IMA NYU Shanghai
+// Interaction Lab
+// For sending multiple values from Arduino to Processing
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensor1 = analogRead(A2);
+  int sensor2 = analogRead(A1);
+
+  // keep this format
+  Serial.print(sensor1);
+  Serial.print(",");  // put comma between sensor values
+  Serial.print(sensor2);
+  Serial.println(); // add linefeed after sending the last sensor value
+
+  // too fast communication might cause some latency in Processing
+  // this delay resolves the issue.
+  delay(100);
+}
